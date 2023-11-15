31014e31da352c77c835399edec3fac63743fc9f
Wzemczak <Wzemczak@gmail.com>
2023-11-15 10:24:31 +0100

    done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
-console.log(galleryItems);
+const ul = document.querySelector(".gallery");
+
+galleryItems.forEach(({ preview, original, description }) => {
+  const li = document.createElement("li");
+  li.innerHTML = `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </div>`;
+
+  ul.appendChild(li);
+});
+
+new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+ul.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.classList.contains("gallery__image")) {
+  }
+});
